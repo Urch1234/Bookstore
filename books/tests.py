8f97a9debe8c580b8a90a20f5b9b7ff93697a8e4
Ihezie Ernest <iernest61@gmail.com>
2024-01-25 07:53:12 +0100

    modified admin models testbook_detail run migration

--- books/tests.py
+++ books/tests.py
@@ -1,17 +1,30 @@
+from django.contrib.auth import get_user_model  # new
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review  # new
 
-# Create your tests here.
+
 class BookTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(  # new
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="testpass123",
+        )
+
         cls.book = Book.objects.create(
             title="Harry Potter",
             author="JK Rowling",
             price="25.00",
+        )
+
+        cls.review = Review.objects.create(  # new
+            book=cls.book,
+            author=cls.user,
+            review="An excellent review",
         )
 
     def test_book_listing(self):
@@ -27,8 +40,9 @@ class BookTests(TestCase):
 
     def test_book_detail_view(self):
         response = self.client.get(self.book.get_absolute_url())
-        no_response = self.client.get("/books/12345")
+        no_response = self.client.get("/books/12345/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")  # new
         self.assertTemplateUsed(response, "books/book_detail.html")
