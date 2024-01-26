4dc1ec7e012044ec610f9d137aa0d363963dda66
Ihezie Ernest <iernest61@gmail.com>
2024-01-26 14:07:23 +0100

    Test file, permissions updated, though 2 test failing

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model  # new
-from django.test import TestCase
+from django.contrib.auth.models import Permission   # new
+from django.test import TestCase, Client
 from django.urls import reverse
 
 from .models import Book, Review  # new
@@ -12,8 +13,12 @@ class BookTests(TestCase):
         cls.user = get_user_model().objects.create_user(  # new
             username="reviewuser",
             email="reviewuser@email.com",
-            password="testpass123",
+            password="testpass123"
         )
+
+        cls.special_permission = Permission.objects.get(
+            codename="special_status"
+        ) # new
 
         cls.book = Book.objects.create(
             title="Harry Potter",
@@ -22,9 +27,9 @@ class BookTests(TestCase):
         )
 
         cls.review = Review.objects.create(  # new
-            book=cls.book,
-            author=cls.user,
-            review="An excellent review",
+            book = cls.book,
+            author = cls.user,
+            review = "An excellent review",
         )
 
     def test_book_listing(self):
@@ -32,13 +37,28 @@ class BookTests(TestCase):
         self.assertEqual(f"{self.book.author}", "JK Rowling")
         self.assertEqual(f"{self.book.price}", "25.00")
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123")
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, "books/book_list.html")
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, "%s?next=/books/" % (reverse("account_login"))
+        )
+        response = self.client.get(
+            "%s?next=/books/" % (reverse("account_login"))
+        )
+        self.assertContains(response, "Log In")
+
+    def test_book_detail_view_with_permissions(self):   # new
+        self.client.login(email="reviewuser@email.com", password="testpass123")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get("/books/12345/")
         self.assertEqual(response.status_code, 200)
--- bookstoreProject/settings.py
+++ bookstoreProject/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  # added
 STATIC_ROOT = BASE_DIR / "staticfiles"  # added
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # added
+MEDIA_URL = "/media/"   # added
+MEDIA_ROOT = BASE_DIR / "media" # added
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
